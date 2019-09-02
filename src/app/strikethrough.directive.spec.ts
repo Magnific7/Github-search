e13d7c49bc2e3d25229a304329b51bf55d988e84
Rithaven <veneruth4@gmail.com>
2019-09-02 13:39:12 +0200

    thenm

--- src/app/strikethrough.directive.spec.ts
+++ src/app/strikethrough.directive.spec.ts
@@ -1,8 +1,9 @@
 import { StrikethroughDirective } from './strikethrough.directive';
+import { ElementRef } from '@angular/core';
 
 describe('StrikethroughDirective', () => {
   it('should create an instance', () => {
-    const directive = new StrikethroughDirective();
+    const directive = new StrikethroughDirective(name);
     expect(directive).toBeTruthy();
   });
 });
